b744b50dae1c83d660113544e4ae1f00cf927739
alelombaa <62141003+alelombaa@users.noreply.github.com>
2022-09-05 01:47:52 +0200

    Add files via upload

--- script/m/rho.m
+++ script/m/rho.m
@@ -1,11 +1,25 @@
-function rho(level,L)
-[t,r]=textread('Arho.txt',"%f %f");
-h = L / 2^level;
-rho = -r*0.5*h;
-figure
+function rho(level,L=0.001)
 
-plot(t,rho)
+f = fopen('Arho.txt');
+N = textscan(f,'%d',2);
+N_v = N{1}(1);
+N_t = N{1}(2);
+
+C = textscan(f,'%f');
+C = reshape(C{1},[N_v,N_t])';
+
+h = L ./ 2.^level;
+
+figure
 xlabel('t [s]')
-ylabel('Q [C]')
+ylabel('\sigma [C/m^2]')
+hold on
+grid on
+xlim([0,max(C(:,1))])
+for i=2:N_v
+	plot(C(:,1),C(:,i)*0.5*h(i-1),'DisplayName',strcat('\sigma_',num2str(i-1)))
+end
+legend('Location','northeast')
 
 end
+
