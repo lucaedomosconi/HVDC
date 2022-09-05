94f0fbabe07c93353b9e4716b6815083db164624
alelombaa <62141003+alelombaa@users.noreply.github.com>
2022-09-05 14:07:30 +0200

    Update export_phi_rho.m

--- script/m/export_phi_rho.m
+++ script/m/export_phi_rho.m
@@ -1,4 +1,4 @@
-function export (final_step, n_procs)
+function export_phi_rho (final_step, n_procs)
 
 	export_tmesh_data ('model_0_u_%04d_%04d', {'model_0_u_%04d_%04d'}, {'phi'}, {}, {},
 	                  "out_u", 0:final_step, n_procs, false);
